cb777aa4efac67cbb7e31b418b17fe352252a489
Jamil Sinno <jamil.sinno@gmail.com>
2022-10-12 14:28:26 -0300

    added leetcode readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # leetcode
+This file is to track all the leetcode problems I have solved. 
+
+![Leetcode profile](/profile.png)
+
+## How It's Made:
+
+Everyday I solve at least one leetcode problem.
+
+**Tech used:** JavaScript
+
+## Optimizations
+I sometimes go back to old solutions and refractor my code.
+
+## Lessons Learned:
+The biggest I learned was how to break down a question. Some questions have specific wording that will change the way you solve the problem. I also learned how to manage my frustration when code doesn't work.   
---
+++ twosum.js
@@ -0,0 +1,27 @@
+// Passed an array and a number. The array will be in a specific order. The order doesn't actually matter, but the values in the array will be all numbers. There will be no special cases in the array. No strings or special characters. The array will always have a minimum length of 2
+
+// We will also be passed a target number to determine if two values from the original array add up to the target value
+
+// Return the index of both numbers if no numbers exist return false
+
+var twoSum = function(nums, target) {
+    // create a hashmap
+    let hashmap = {}
+
+    // loop through the array and determine if the difference shows up in the hashmap
+    for (element in nums) {
+        let otherNum = target - nums[element]
+        if(otherNum in hashmap){
+            return [nums.indexOf(otherNum), +element]
+        }
+        // set each element in the array to its index
+        hashmap[nums[element]] = element
+    }
+
+    return false
+
+};
+
+console.log(twoSum([2,7,11,15], 9), [0,1])
+console.log(twoSum([3,2,4], 7), [0,2])
+console.log(twoSum([3,3], 6), [0,1])
