92a7907384727ea4e0c24ddb1aee365a6755141e
Jamil Sinno <jamil.sinno@gmail.com>
2022-12-12 11:46:32 -0400

    solved arrayFromPermutation

--- arrayFromPermutation.js
+++ arrayFromPermutation.js
@@ -1,10 +1,25 @@
+// var buildArray = function(nums) {
+//   let ans = []
+//   for (let i = 0; i <= nums.length - 1; i++) {
+//       ans.push(nums[nums[i]])
+//   }  
+// //   console.log(ans)
+//   return ans
+// };
+
 var buildArray = function(nums) {
-  let ans = []
-  for (let i = 0; i <= nums.length - 1; i++) {
-      ans.push(nums[nums[i]])
-  }  
-//   console.log(ans)
-  return ans
+    let l = nums.length
+    for(let i=0;i<nums.length;i++){
+        // console.log(nums[i], i)
+        nums[i]=nums[i]+(nums[nums[i]]%l)*l
+        // console.log(nums[i], i)
+    }
+    for(let i=0;i<nums.length;i++){
+        console.log(nums[i], i)
+        nums[i]=Math.floor(nums[i]/l)
+        console.log(nums[i], i)
+    }
+    return nums; 
 };
 
 console.log( buildArray([0,2,1,5,3,4]), [0,1,2,4,5,3]) 
