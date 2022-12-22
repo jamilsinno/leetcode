d5526c3d46974efdc558a6a99acc625f3ddb6b5e
Jamil Sinno <jamil.sinno@gmail.com>
2022-12-22 10:41:35 -0400

    solved maxNumberOfWordsInSentences

---
+++ maxNumberOfWordsFoundInSentence.ts
@@ -0,0 +1,13 @@
+function mostWordsFound(sentences: string[]): number {
+    let max: number = 0;
+    let len: number = 0;
+    for ( let i = 0; i < sentences.length; i++ ) {
+        if (sentences[i].split(' ').length > max) {
+            len = sentences[i].split(' ').length;
+            max = len;
+        };
+    };
+    return len;
+};
+
+console.log(mostWordsFound(["w jrpihe zsyqn l dxchifbxlasaehj","nmmfrwyl jscqyxk a xfibiooix xolyqfdspkliyejsnksfewbjom","xnleojowaxwpyogyrayfgyuzhgtdzrsyococuqexggigtberizdzlyrdsfvryiynhg","krpwiazoulcixkkeyogizvicdkbrsiiuhizhkxdpssynfzuigvcbovm","rgmz rgztiup wqnvbucfqcyjivvoeedyxvjsmtqwpqpxmzdupfyfeewxegrlbjtsjkusyektigr","o lgsbechr lqcgfiat pkqdutzrq iveyv iqzgvyddyoqqmqerbmkxlbtmdtkinlk","hrvh efqvjilibdqxjlpmanmogiossjyxepotezo","qstd zui nbbohtuk","qsdrerdzjvhxjqchvuewevyzlkyydpeeblpc"])) // 6
