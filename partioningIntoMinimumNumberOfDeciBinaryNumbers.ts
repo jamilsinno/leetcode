9fe46e67d1617d472ae453d0f680a8445eaf53f9
Jamil Sinno <jamil.sinno@gmail.com>
2023-01-15 00:04:17 -0400

    partitioningIntoMinimumNumberOfDeciBinaryNumbers

--- partioningIntoMinimumNumberOfDeciBinaryNumbers.ts
+++ partioningIntoMinimumNumberOfDeciBinaryNumbers.ts
@@ -1,41 +1,12 @@
-function numberOfMatches(n: number): number {
-    // let matches: number = n % 2 === 0 ? n / 2 : ( n - 1 ) / 2;
-
-    // let count: number += matches;
-    // console.log(count)
-    // if (n === 2 || matches === 1) {
-    //     return count
-    // } else {
-    //     n % 2 === 0 ? n = n / 2 : n = ((n-1) / 2) + 1
-    // }
-
-    // numberOfMatches(n)
-
-    // let matches: number;
-    // n % 2 === 0 ? matches = n / 2 : matches = (n - 1) / 2
-
-    // let count: number = matches;
-    
-    // if (matches === 1 && n % 2 !== 0) {
-    //     return count = 2
-    // } else if (matches === 1 && n === 2) {
-    //     return count = 1
-    // }
-
-    // console.log(n, matches, count)
-    // do {
-    //     if (n <= 2) break
-    //     if (n % 2 === 0) {
-    //         matches = n / 2
-    //         n = n / 2
-    //     } else {
-    //         matches = (n - 1) / 2
-    //         n = (n - 1) / 2 + 1
-    //     }
-    //     count += matches
-    //     console.log(n, matches, count)
-    // } while (n > 2) 
-
-    // return count
-    return n - 1
+function numberOfMatches(n: number, count:number = 0): number{
+    count += n % 2 === 0 ? n / 2 : ( n - 1 ) / 2;
+    if (n < 2) {
+        return count
+    }else {
+        n % 2 === 0 ? n = n / 2 : n = ((n-1) / 2 + 1)
+        return numberOfMatches(n, count)
+    }
+    // return n - 1
 };
+
+numberOfMatches(7)
