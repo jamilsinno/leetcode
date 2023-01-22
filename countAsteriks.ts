8a9a869e0048eb23e636445b27aa748ddc9cff0b
Jamil Sinno <101027036+jamilsinno@users.noreply.github.com>
2023-01-22 12:21:45 -0400

    Added video solution

--- countAsteriks.ts
+++ countAsteriks.ts
@@ -1,14 +1,23 @@
-function countAsterisks(s: string): number {
-    let pair: boolean = false;
-    let count: number = 0;
+// We're being passed a string with alphanumeric characters. The characters can be upper/lower case. The string will contain | & * and the vertical bars indicate which asterik we need to count.
 
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === '|') {
+// return the total number of asteriks that fall outside of vertical bar pairs
+
+function countAsterisks(s: string): number {
+    let count: number = 0;
+    let pair: boolean = false;
+
+    // iterate over our loop, determine when you're outside of a pair of vertical bars and count those asteriks
+    for (let i: number = 0; i < s.length; i++) {
+        // check if you are in a pair
+        if ( s[i] === '|' ) {
             pair = !pair
         }
-        if ( s[i] === '*' && pair === false ){
+
+        // Add to the total count if the asterik falls outside of a pair
+        if ( s[i] === '*' && pair === false) {
             count += 1
         }
     }
+
     return count
 };
