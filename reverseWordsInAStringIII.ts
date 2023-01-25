b3918dc28b970e2b6593379b91b7fadf1961de59
Jamil Sinno <jamil.sinno@gmail.com>
2023-01-25 14:10:00 -0400

    updated reverseString with video solution

--- reverseWordsInAStringIII.ts
+++ reverseWordsInAStringIII.ts
@@ -1,12 +1,15 @@
+// Passed a string that contains alphanumeric characters. There will be no leading or trailing spaces. There will be no arrays or objects for us to worry about. There will always be at least one word for us to reverse in the string. 
+
+// Return the same string but have every word reversed. Reverse the characters of each word while maintaining the original place in the string.
+
 function reverseWords(s: string): string {
-    // split the string to work with arrays
-    let arr = s.split(' ');
+    // split each word that is passed in the string. 
+    let ans: string[] = s.split(' ')
 
-    // iterate over the array, reassigning each index with its reverse variant
-    for ( let i = 0; i < arr.length; i++ ) {
-        arr[i] = arr[i].split('').reverse().join('');
+    // iterate over the whole string, and individually reverse every word
+    for ( let i: number = 0; i < ans.length; i++ ) {
+        ans[i] = ans[i].split('').reverse().join('')
     }
-
-    // join the array back into a string and return
-    return arr.join(' ')
+    // return the string separated by spaces again
+    return ans.join(' ')
 };
