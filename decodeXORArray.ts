27604c14fa7544a4cfcfa962511231ccd5d0a776
jamilsinno <jamil.sinno@gmail.com>
2023-01-28 12:21:13 -0500

    solved decodeXORArray

---
+++ decodeXORArray.ts
@@ -0,0 +1,9 @@
+function decode(encoded: number[], first: number): number[] {
+    let ans = [first];
+
+    for (let i: number = 0; i < encoded.length; i++) {
+        ans.push(ans[i] ^ encoded[i])
+    }
+    
+    return ans
+};
