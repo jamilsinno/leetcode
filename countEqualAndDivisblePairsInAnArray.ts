4023f3c84f2532514c61c164875ce3a338f0435c
Jamil Sinno <101027036+jamilsinno@users.noreply.github.com>
2023-02-09 15:50:05 -0400

    Update countEqualAndDivisblePairsInAnArray.ts

--- countEqualAndDivisblePairsInAnArray.ts
+++ countEqualAndDivisblePairsInAnArray.ts
@@ -1,13 +1,22 @@
-function countPairs(nums: number[], k: number): number {
-    let counter: number = 0; 
+// We are going to be passed an integer array. With this array there will also be variable k, a number which is  passed. What we need to do is find valid pairs that are equal and the product of their indices is divisible by k
 
-    for (let i = 0; i < nums.length; i++) {
-        for (let j: number = i + 1; j < nums.length; j++) {
-            if ( nums[i] === nums[j] && (i * j) % k === 0 ) {
-                counter += 1;
+// Return the total count of valid pairs found in the numbers array.
+
+function countPairs(nums: number[], k: number): number {
+    // Create a variable to hold your count in
+    let count: number = 0;
+
+    // iterate over our out loop
+    for ( let i: number = 0; i < nums.length; i++ ) {
+        // iterate to get the numbers we are comparing with
+        for ( let j: number = i + 1; j < nums.length; j++ ) {
+            // check your condition and if it passes add to your counter
+            if ( (nums[i] === nums[j]) && ((i * j) % k === 0)) {
+                count += 1;
             }
         }
     }
 
-    return counter
+    // return the total count
+    return count
 };
