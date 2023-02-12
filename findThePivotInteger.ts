0154e50060beceed7eb00d69ae72c09de0adcc0c
Jamil Sinno <101027036+jamilsinno@users.noreply.github.com>
2023-02-12 18:58:36 -0400

    Update findThePivotInteger.ts

--- findThePivotInteger.ts
+++ findThePivotInteger.ts
@@ -1,19 +1,28 @@
-function pivotInteger(n: number): number {
-    if ( n === 1 ) return 1
-    let totalSum: number = 0;
-    let previousSum: number = 0
+4// We are going to be passed a number. Using this number we are supposed to find if there is a pivot integer
 
+// If there a pivot integer, return it. If n is 1, return 1. If there is no pivot integer, then return -1. 
+
+function pivotInteger(n: number): number {
+    // Create a variable to hold your total sum
+    let totalSum: number = 0;
+    // Create a variable to hold your previous sum
+    let previousSum: number = 0;
+
+    // figure out the total sum
     for (let i: number = 1; i <= n; i++) {
-        totalSum += i
+        totalSum += i   
     }
 
+    // Find out if the total sum is equal the previous sum plus the current index to find the pivot integer
     for (let i: number = 1; i <= n; i++) {
-        if (totalSum === previousSum+i) {
+        console.log({totalSum}, previousSum + i, {previousSum}, i)
+        if ( totalSum === previousSum + i ) {
             return i
         }
         totalSum -= i
         previousSum += i
     }
 
+    // If any of the numbers aren't a pivot integer, return -1
     return -1
 };
