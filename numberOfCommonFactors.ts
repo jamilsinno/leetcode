a6140ff24b99560314d99ee8df8a901c7e2ef191
Jamil Sinno <101027036+jamilsinno@users.noreply.github.com>
2023-02-13 15:31:15 -0400

    Update numberOfCommonFactors.ts

--- numberOfCommonFactors.ts
+++ numberOfCommonFactors.ts
@@ -1,12 +1,22 @@
+// We are going to be passed two positive numbers. Both numbers will have common factors. A might be bigger than b or b might be bigger than a. We are not going to be passed any strings. We also are not being passed an array. We will also not be seeing any numbers with decimals. They will always be whole numbers.
+
+// Return the total number of common factors between the two passed parameters.
+
 function commonFactors(a: number, b: number): number {
-    let min: number = Math.min(a, b)
+    // Figure out which number between parameters a & b is smaller and assign that as your maximum common factor
+    let maxCommonFactor: number = Math.min(a, b)
+    // Create a variable to hold the total number of common factors you have
     let count: number = 0;
 
-    for (let i = 1; i <= min; i++) {
-        if (a % i === 0 && b % i === 0) {
-            count += 1
+    // Iterate over all possible positive integers from 1 to maxCommonFactor and count all the common factors
+    for (let i: number = 1; i <= maxCommonFactor; i++) {
+        // Check to see if a or b divided by i is a whole number and if it is count it
+        // console.log({a},{b},{i}, a % i, b % i)
+        if  (a % i === 0 && b % i === 0) {
+            count += 1;
         }
     }
 
+    // Return the total count
     return count
 };
