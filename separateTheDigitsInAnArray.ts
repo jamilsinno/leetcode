25da6998ab0f645302d5b981d31fc2878b2b1557
Jamil Sinno <101027036+jamilsinno@users.noreply.github.com>
2023-02-17 15:18:04 -0400

    Update separateTheDigitsInAnArray.ts

--- separateTheDigitsInAnArray.ts
+++ separateTheDigitsInAnArray.ts
@@ -1,3 +1,10 @@
+// We are going to be passed an array of numbers. The array of numbers will consist of numbers up to 1000. There will be no alphabet character or special or characters or nested arrays. 
+
+// We need return the array in the same order but have every digit separated into its own index. 
+
 function separateDigits(nums: number[]): number[] {
-    return nums.map( x => String(x) ).join('').split('').map( x => +x )
- };
+    // turn the numbers in the array into a string
+    // join the numbers into one large string and then split them
+    // take the answer and turn it back into type number and return it
+    return nums.map( el => String(el) ).join('').split('').map( el => Number(el) )
+};
