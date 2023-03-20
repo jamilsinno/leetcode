23d0d9a8305b6d71922da9b868a923f9151cedb6
Jamil Sinno <jamil.sinno@gmail.com>
2023-03-20 09:39:00 -0300

    solved leetcode

---
+++ findTargetIndicesAfterSortingArray.ts
@@ -0,0 +1,13 @@
+function targetIndices(nums: number[], target: number): number[] {
+    let list = [];
+
+    nums.sort((a,b) => a -b);
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            list.push(i)
+        }
+    }
+
+    return list
+};
